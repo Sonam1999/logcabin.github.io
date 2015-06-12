627988fff42978cc0597787f793335177ca9523a
Documentation Builder <ongardie+automated@gmail.com>
2015-06-12 21:14:07 +0000

    Automated documentation build for logcabin/logcabin@99e3840

--- doxygen/search/variables_69.js
+++ doxygen/search/variables_69.js
@@ -8,6 +8,7 @@ var searchData=
   ['invariants',['invariants',['../classLogCabin_1_1Server_1_1RaftConsensus.html#a2d6ee7995b60660085fdf042d07c276c',1,'LogCabin::Server::RaftConsensus']]],
   ['is_5fsteady',['is_steady',['../structLogCabin_1_1Core_1_1Time_1_1CSystemClock.html#a7a715091394fc942bae20fa03ee50121',1,'LogCabin::Core::Time::CSystemClock::is_steady()'],['../structLogCabin_1_1Core_1_1Time_1_1CSteadyClock.html#adc96c0781a8f555a304a38eb92400794',1,'LogCabin::Core::Time::CSteadyClock::is_steady()'],['../structLogCabin_1_1Core_1_1Time_1_1MockableClock.html#ab4f4cd42e83e3568af0feffc206851e1',1,'LogCabin::Core::Time::MockableClock::is_steady()']]],
   ['iscaughtup_5f',['isCaughtUp_',['../classLogCabin_1_1Server_1_1RaftConsensusInternal_1_1Peer.html#aa35915b9016d63af612039b1eea9c9bf',1,'LogCabin::Server::RaftConsensusInternal::Peer']]],
+  ['isconnecting',['isConnecting',['../classLogCabin_1_1Client_1_1LeaderRPC.html#a14bb81a36c5ac6423a158fe78738ac34',1,'LogCabin::Client::LeaderRPC']]],
   ['isloggingcache',['isLoggingCache',['../namespaceLogCabin_1_1Core_1_1Debug_1_1Internal.html#a87e5b990cfa0531a01de3e2bfd26c9fb',1,'LogCabin::Core::Debug::Internal']]],
   ['isopen',['isOpen',['../structLogCabin_1_1Storage_1_1SegmentedLog_1_1Segment.html#af3705bc3ed181a60ee2b7b91bc5bc9cb',1,'LogCabin::Storage::SegmentedLog::Segment']]]
 ];
