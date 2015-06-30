dba777393f98962578067cef4de4a955d2d734d1
Documentation Builder <ongardie+automated@gmail.com>
2015-06-30 00:45:53 +0000

    Automated documentation build for logcabin/logcabin@381268f

--- doxygen/search/classes_65.js
+++ doxygen/search/classes_65.js
@@ -3,6 +3,7 @@ var searchData=
   ['entry',['Entry',['../structLogCabin_1_1Server_1_1RaftConsensus_1_1Entry.html',1,'LogCabin::Server::RaftConsensus']]],
   ['exactlyoncerpchelper',['ExactlyOnceRPCHelper',['../classLogCabin_1_1Client_1_1ClientImpl_1_1ExactlyOnceRPCHelper.html',1,'LogCabin::Client::ClientImpl']]],
   ['exception',['Exception',['../classLogCabin_1_1Client_1_1Exception.html',1,'LogCabin::Client']]],
+  ['exception',['Exception',['../structLogCabin_1_1Core_1_1Config_1_1Exception.html',1,'LogCabin::Core::Config']]],
   ['exithandler',['ExitHandler',['../classLogCabin_1_1Server_1_1Globals_1_1ExitHandler.html',1,'LogCabin::Server::Globals']]],
   ['expected',['Expected',['../structLogCabin_1_1RPC_1_1ServiceMock_1_1Expected.html',1,'LogCabin::RPC::ServiceMock']]]
 ];
--- doxygen/search/defines_65.js
+++ doxygen/search/defines_65.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['error',['ERROR',['../Core_2Debug_8h.html#a78e40dfd87f516e322340d249027aea1',1,'Debug.h']]],
+  ['exit',['EXIT',['../Core_2Debug_8h.html#ae62d555ee2a777ff1ff54e6c6175891c',1,'Debug.h']]],
   ['expect',['expect',['../RaftConsensusInvariants_8cc.html#abdef3abb58093f62db373efcbc4877e5',1,'RaftConsensusInvariants.cc']]]
 ];
