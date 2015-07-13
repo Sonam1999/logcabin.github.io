74c36eaf123641a17ca17f6e214ba6752cd4f992
Documentation Builder <ongardie+automated@gmail.com>
2015-07-13 19:47:48 +0000

    Automated documentation build for logcabin/logcabin@a7986dc

--- doxygen/search/typedefs_73.js
+++ doxygen/search/typedefs_73.js
@@ -4,7 +4,7 @@ var searchData=
   ['serverid',['ServerId',['../classLogCabin_1_1Client_1_1SessionManager.html#adaef23be09b3d87b26f38d93ee493c79',1,'LogCabin::Client::SessionManager']]],
   ['serverref',['ServerRef',['../classLogCabin_1_1Server_1_1RaftConsensusInternal_1_1Configuration.html#aa59cb8c3759ba19620d16bd6a71bdb02',1,'LogCabin::Server::RaftConsensusInternal::Configuration']]],
   ['sideeffect',['SideEffect',['../classLogCabin_1_1Server_1_1RaftConsensusInternal_1_1Configuration.html#abf2bb86fbae1483909a8889762df58a5',1,'LogCabin::Server::RaftConsensusInternal::Configuration']]],
-  ['steadyclock',['SteadyClock',['../namespaceLogCabin_1_1Core_1_1Time.html#a4798c7e2244561626e4ce819710e7e56',1,'LogCabin::Core::Time']]],
+  ['steadyclock',['SteadyClock',['../classLogCabin_1_1Server_1_1ServerStats.html#adbdfc96e7622578efd96d23a3f3a291e',1,'LogCabin::Server::ServerStats::SteadyClock()'],['../namespaceLogCabin_1_1Core_1_1Time.html#a4798c7e2244561626e4ce819710e7e56',1,'LogCabin::Core::Time::SteadyClock()']]],
   ['string',['string',['../classLogCabin_1_1Core_1_1Config.html#a1404aa63c0c39039017dabab64495274',1,'LogCabin::Core::Config']]],
-  ['systemclock',['SystemClock',['../namespaceLogCabin_1_1Core_1_1Time.html#a9edb96154f308f4284060230766da1f1',1,'LogCabin::Core::Time']]]
+  ['systemclock',['SystemClock',['../classLogCabin_1_1Server_1_1ServerStats.html#a0e4ec39d93aee910c4f82b884f27b449',1,'LogCabin::Server::ServerStats::SystemClock()'],['../namespaceLogCabin_1_1Core_1_1Time.html#a9edb96154f308f4284060230766da1f1',1,'LogCabin::Core::Time::SystemClock()']]]
 ];
--- doxygen/search/variables_69.js
+++ doxygen/search/variables_69.js
@@ -4,7 +4,6 @@ var searchData=
   ['inbound',['inbound',['../classLogCabin_1_1RPC_1_1MessageSocket.html#aa9723a90aa0df6884e1ccfc32a24b232',1,'LogCabin::RPC::MessageSocket']]],
   ['index',['index',['../structLogCabin_1_1Server_1_1RaftConsensus_1_1Entry.html#ae53c9259efad84029cd9c7b0b25288bd',1,'LogCabin::Server::RaftConsensus::Entry']]],
   ['init',['init',['../classLogCabin_1_1Core_1_1Random_1_1anonymous__namespace_02Random_8cc_03_1_1RandomState.html#ad43583c675ec84527140d6569f98b19f',1,'LogCabin::Core::Random::anonymous_namespace{Random.cc}::RandomState']]],
-  ['intervalnanos',['intervalNanos',['../classLogCabin_1_1Server_1_1ServerStats_1_1TimerHandler.html#adde0bf4e9deee47d3be72d4d0ecaab97',1,'LogCabin::Server::ServerStats::TimerHandler']]],
   ['invariants',['invariants',['../classLogCabin_1_1Server_1_1RaftConsensus.html#a2d6ee7995b60660085fdf042d07c276c',1,'LogCabin::Server::RaftConsensus']]],
   ['is_5fsteady',['is_steady',['../structLogCabin_1_1Core_1_1Time_1_1CSystemClock.html#a7a715091394fc942bae20fa03ee50121',1,'LogCabin::Core::Time::CSystemClock::is_steady()'],['../structLogCabin_1_1Core_1_1Time_1_1CSteadyClock.html#adc96c0781a8f555a304a38eb92400794',1,'LogCabin::Core::Time::CSteadyClock::is_steady()'],['../structLogCabin_1_1Core_1_1Time_1_1MockableClock.html#ab4f4cd42e83e3568af0feffc206851e1',1,'LogCabin::Core::Time::MockableClock::is_steady()']]],
   ['isblocked',['isBlocked',['../classLogCabin_1_1Event_1_1Signal_1_1Blocker.html#a289b5e8e0aed011e8015a1167c9788f6',1,'LogCabin::Event::Signal::Blocker']]],
@@ -12,5 +11,6 @@ var searchData=
   ['isconnecting',['isConnecting',['../classLogCabin_1_1Client_1_1LeaderRPC.html#a14bb81a36c5ac6423a158fe78738ac34',1,'LogCabin::Client::LeaderRPC']]],
   ['isloggingcache',['isLoggingCache',['../namespaceLogCabin_1_1Core_1_1Debug_1_1Internal.html#a87e5b990cfa0531a01de3e2bfd26c9fb',1,'LogCabin::Core::Debug::Internal']]],
   ['isopen',['isOpen',['../structLogCabin_1_1Storage_1_1SegmentedLog_1_1Segment.html#af3705bc3ed181a60ee2b7b91bc5bc9cb',1,'LogCabin::Storage::SegmentedLog::Segment']]],
-  ['issnapshotrequested',['isSnapshotRequested',['../classLogCabin_1_1Server_1_1StateMachine.html#a9faac0c9d6e607948d232d07e4de2da8',1,'LogCabin::Server::StateMachine']]]
+  ['issnapshotrequested',['isSnapshotRequested',['../classLogCabin_1_1Server_1_1StateMachine.html#a9faac0c9d6e607948d232d07e4de2da8',1,'LogCabin::Server::StateMachine']]],
+  ['isstatsdumprequested',['isStatsDumpRequested',['../classLogCabin_1_1Server_1_1ServerStats.html#acd2bbff693b1c2b2106ad2f1a2eaa9a7',1,'LogCabin::Server::ServerStats']]]
 ];
