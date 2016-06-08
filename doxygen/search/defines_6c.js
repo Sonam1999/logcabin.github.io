032f8a414669b849cd65453b9b5ae5c80edf8e1d
Documentation Builder <ongardie+automated@gmail.com>
2016-06-08 20:43:43 +0000

    Automated documentation build for logcabin/logcabin@7ae9519

--- doxygen/search/defines_6c.js
+++ doxygen/search/defines_6c.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['log',['LOG',['../Core_2Debug_8h.html#a2d871a9bb0ec3ed46f6450ab2c20a18e',1,'Debug.h']]]
+  ['log',['LOG',['../Core_2Debug_8h.html#a2ebc89ef0e543fcc389d4b43f81a024b',1,'Debug.h']]]
 ];
